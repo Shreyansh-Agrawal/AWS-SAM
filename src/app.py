8b7967d6d21ab9ffc931d47476929b317669e279
Shreyansh Agrawal <Shreyansh.Agrawal@watchguard.com>
2024-04-21 19:02:39 +0530

    SAM API Gateway

--- src/app.py
+++ src/app.py
@@ -1,7 +1,20 @@
+import json
+
 print('loading function')
 
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
 def lambda_handler(event, context):
-    print('Event:\n', event)
+    print('Event:\n' + json.dumps(event, indent=2))
     print('Context:\n', context)
 
-    return 'Hello SAM'
+    return respond(None, res='Hellow SAM from Lambda')
